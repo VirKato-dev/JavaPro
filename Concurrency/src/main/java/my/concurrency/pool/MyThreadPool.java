9b1fd5dca32da84e7460afb901688bf407b3e117
VirKato-dev <dascat79@mail.ru>
2024-04-13 11:20:33 +0300

    HW4 with the interrupt flag

--- Concurrency/src/main/java/my/concurrency/pool/MyThreadPool.java
+++ Concurrency/src/main/java/my/concurrency/pool/MyThreadPool.java
@@ -8,7 +8,6 @@ public class MyThreadPool {
     private final int numberOfWorkers;
     private final Thread[] workers;
     private final LinkedList<Runnable> taskQueue = new LinkedList<>();
-    private volatile boolean active = true;
 
 
     public MyThreadPool(int numberOfWorkers) {
@@ -19,7 +18,7 @@ public class MyThreadPool {
 
 
     public synchronized void execute(Runnable task) {
-        if (!active) {
+        if (Thread.currentThread().isInterrupted()) {
             throw new IllegalStateException();
         }
         taskQueue.add(task);
@@ -28,7 +27,9 @@ public class MyThreadPool {
 
 
     public void shutdown() {
-        active = false;
+        for (Thread worker : workers) {
+            worker.interrupt();
+        }
     }
 
 
@@ -36,7 +37,7 @@ public class MyThreadPool {
         for (int i = 0; i < numberOfWorkers; i++) {
 
             workers[i] = new Thread(() -> {
-                while (!taskQueue.isEmpty() || active) {
+                while (!taskQueue.isEmpty() || !Thread.currentThread().isInterrupted()) {
                     nextTask().ifPresent(Runnable::run);
                 }
             });
