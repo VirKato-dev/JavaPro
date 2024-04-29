bc751566869cfa6f305dc3d8d84159da76b252d7
VirKato-dev <dascat79@mail.ru>
2024-04-29 10:22:48 +0300

    fix null HW5 patterns Iterator

--- Patterns/src/main/java/virkato/otus/iterator/Box.java
+++ Patterns/src/main/java/virkato/otus/iterator/Box.java
@@ -1,15 +1,13 @@
 package virkato.otus.iterator;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 
 public class Box {
-    private final List<String> list1;
-    private final List<String> list2;
-    private final List<String> list3;
-    private final List<String> list4;
+    private final List<String> list1 = new ArrayList<>();
+    private final List<String> list2 = new ArrayList<>();
+    private final List<String> list3 = new ArrayList<>();
+    private final List<String> list4 = new ArrayList<>();
 
     private class MyIterator implements Iterator<String> {
         private final Iterator<String> it1 = list1.iterator();
@@ -45,9 +43,9 @@ public class Box {
 
 
     public Box(List<String> list1, List<String> list2, List<String> list3, List<String> list4) {
-        this.list1 = list1;
-        this.list2 = list2;
-        this.list3 = list3;
-        this.list4 = list4;
+        this.list1.addAll(list1 != null ? list1 : Collections.emptyList());
+        this.list2.addAll(list2 != null ? list2 : Collections.emptyList());
+        this.list3.addAll(list3 != null ? list3 : Collections.emptyList());
+        this.list4.addAll(list4 != null ? list4 : Collections.emptyList());
     }
 }
